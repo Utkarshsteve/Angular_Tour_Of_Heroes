99531c8fd93eb3ca28f691cc29f46db1a24d31a9
Utkarshsteve <utkarshsteve7@gmail.com>
2021-05-17 10:57:00 +0530

    @Input() decorator for data to flow from parent to child component

---
+++ src/app/hero-detail/hero-detail/hero-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { IHero } from 'src/app/hero';
+
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero?: IHero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
